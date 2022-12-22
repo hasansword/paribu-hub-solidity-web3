bfcfe324555038e55bc1cf3d5b884a8486452959
Hasan Kılıç <97hasankilic@gmail.com>
2022-12-22 20:22:48 +0300

    Counter at Solidity have two function increment and decrement

---
+++ Challenges/Counter.sol
@@ -0,0 +1,13 @@
+//SPDX-Identifier-License: MIT
+pragma solidity ^0.8.7;
+
+contract Counter {
+    uint public count;
+    function increment() external {
+        count += 1;
+    }
+
+    function decrement() external {
+        count -= 1;
+    }
+}
