0d26fe7e00dfb238dac8e93cae6a7a0b183ae1eb
Hasan Kılıç <97hasankilic@gmail.com>
2022-12-27 16:08:47 +0300

    How immutable works is shown.

--- Lessons/Immutable.sol
+++ Lessons/Immutable.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+//Immutables are variables that cannot be modified. 
+//The difference from Constant is that the value of the variable marked with 
+//immutable can be changed at the beginning with the constructor.
 contract Immutable{
     // 45718 gas cost without immutable
     // 43585 gas cost with immutable 
